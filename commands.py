3d97ad4c70aa06a9d9707ba707c33f2c761adbbc
Scott Malone <scottmcarpentry@gmail.com>
2020-12-16 18:37:38 +1000

    Successfully build create, drop and seed databases

--- commands.py
+++ commands.py
@@ -2,9 +2,6 @@ from flask import Blueprint
 from main import db
 db_commands = Blueprint("db",__name__)
 
-# from faker import Faker
-# fake = Faker()
-
 @db_commands.cli.command("create")
 def create_db():
     db.create_all()                     # Create_all() method is from the SQLAlchemy
@@ -16,5 +13,29 @@ def drop_db():
     db.drop_all()
     print("Tables dropped")
 
-# @db_commands.cli.command("seed")
-# def seed_db():
+@db_commands.cli.command("seed")
+def seed_db():
+    from models.User_table import User
+    from faker import Faker
+    import random
+
+    fake = Faker()
+    
+    for i in range(5):
+        user = User()
+        user.email = fake.email()
+        user.first_name = fake.first_name()
+        user.Surname = fake.first_name()
+        user.Password = fake.password()
+        user.Age = random.randint(18,60)
+        user.Address = fake.address()
+        user.City = fake.city()
+        user.State = fake.state()
+        user.Country = fake.country()
+        user.Postcode = random.randint(1000+i,3000)
+        db.session.add(user)
+        db.session.commit()
+    print("Table seeded")
+
+
+
