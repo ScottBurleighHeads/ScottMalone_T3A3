813d20c192120675ddfd131a78261545c07d1c64
Scott Malone <scottmcarpentry@gmail.com>
2020-12-28 20:03:36 +1000

    Authorising and completing playlist add/delete track

--- controllers/Playlist_controller.py
+++ controllers/Playlist_controller.py
@@ -13,11 +13,11 @@ playlist = Blueprint("playlist",__name__,url_prefix="/playlist")
 def get_playlist(id):
    
     user1 = User.query.filter_by(id = id).first()
-    playlist = []
+    play = []
     try:
         for tracks in user1.playlist:
-            playlist.append(tracks.tracks_name)
-            user_playlist = {f"{user1.first_name}s playlist":playlist}
+            play.append(tracks.tracks_name)
+            user_playlist = {f"{user1.first_name}s playlist":play}
         return user_playlist
     except: 
         return f"<h3>ERROR: {user1.first_name} does not have a playlist</h3>"
@@ -39,7 +39,18 @@ def add_track():
 
     return {"User": f"{user.first_name} {user.Surname}","New track added":f"{track.tracks_name}","Playlist":f"{get_playlist(user.id)[f'{user.first_name}s playlist']}"}
 
-# @playlist.route("/delete_track<int:id>", method=['DELETE'])
-# @jwt_required
-# def delete_track(id):
-    
+@playlist.route("/delete_track", methods=['DELETE'])
+@jwt_required
+def delete_track():
+    tracks_field = track_schema.load(request.json)
+    track = Tracks.query.filter_by(tracks_id=tracks_field['tracks_id']).first()
+    track_name = track.tracks_name
+    print(track.tracks_id)
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+    user_name = user.first_name + " " + user.Surname
+    print(user.id)
+    track.playlist.remove(user)
+    db.session.commit()
+
+    return f'{track_name} was removed from {user_name} playlist'
--- models/Playlist_table.py
+++ models/Playlist_table.py
@@ -1,11 +1,12 @@
 from main import db
 from sqlalchemy import UniqueConstraint
 
-# Many to many connection between Users and tracks
+# Many to Many connection between Users and tracks. Added UniqueConstraint so the same
+# track cant be added multiple times.
 playlist = db.Table('playlist',
-db.Column('tracks_id',db.Integer, db.ForeignKey('tracks.tracks_id'), nullable=False),
 db.Column('id',db.Integer, db.ForeignKey('user.id'), nullable=False),
-UniqueConstraint('tracks_id','id')
+db.Column('tracks_id',db.Integer, db.ForeignKey('tracks.tracks_id'), nullable=False),
+UniqueConstraint('id','tracks_id')
 )
 
 
